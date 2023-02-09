536b6be43bf99d6da85f3b108db1849307f9a521
James Bao <jambao@pdx.edu>
2023-02-09 00:08:08 -0800

    Added notes from Google Colab notebook
    https://colab.research.google.com/drive/1tmo76dpY9fiJrk2cYHpm4QNbd0JviLOB#scrollTo=wo7vV1bH6ejA
    
    added Python functions and rough plans for implementation
    Update: just modify a copy of the adjacency matrix and apply Prim's algorithm to generate a Minimum Spanning Tree

--- China_surnames.py
+++ China_surnames.py
@@ -8,7 +8,7 @@ import pandas.plotting._matplotlib as plt
 raw = pd.read_csv('China_top10surname.csv', skiprows=1, names=['Province','1','2','3','4','5','6','7','8','9','10'])
 data = np.array(raw)
 
-#rint(raw)
+#print(raw)
 #print(data)
 print()
 
@@ -27,6 +27,14 @@ Create confusion matrix for all 28 provinces
 
 '''
 Map visualization (models the 28 provinces as a minimal spanning tree)
+
+https://www.youtube.com/watch?v=LFT0rj-xvYA for Prim's algorithm
+
+Dec 2022 update- https://www.ecosia.org/search?addon=chrome&addonversion=5.1.2&q=how%20to%20store%20chinese%20characters%20as%20information%20in%20python
+
+Idea- eventually edit each element in the map to be a data structure that contains a province (abbreviated, full English, full Chinese), and a surname 
+(ideally in converted format and with the original character being displayed with pinyin also displayed)...
+
 '''
 
 provinces_map = np.chararray((8,6))
@@ -62,3 +70,95 @@ provinces_map[7,4] = 'O' # Hainan (HAN)
 
 print(provinces_map)
 print()
+
+
+# source: https://i.imgur.com/m8M4Dwj.jpg
+prc_prov_adj_matrix = 
+                    [[0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Manchuria
+                    [0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Shandong
+                    [0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Beijing
+                    [1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Hebei
+                    [0,0,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Shanxi
+                    [0,0,0,1,1,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # InnerMongolia
+                    [0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Tianjin
+                    [0,1,0,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0], # Henan
+                    [0,0,0,0,1,1,0,1,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0], # Shaanxi
+                    [0,0,0,0,0,1,0,0,1,0,1,1,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0], # Gansu
+                    [0,0,0,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # Ningxia
+                    [0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0], # Xinjiang
+                    [0,1,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,1,0,0,0,0], # Anhui
+                    [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0], # Jiangsu
+                    [0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0], # Qinghai
+                    [0,0,0,0,0,0,0,1,1,0,0,0,1,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0], # Hubei
+                    [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,0,0,0,0,0,0], # Chongqing
+                    [0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0], # Sichuan
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,0,0,0,0,0,0,1], # Guizhou
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,1], # Hunan
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1], # Yunnan
+                    [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,1,1,1,0,0], # Jiangxi
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0], # Shanghai
+                    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0], # Zhejiang
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0], # Fujian
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,1,0,1,1], # Guangdong
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0], # Hainan
+                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,0,0]] # Guangxi
+                    
+                    
+# https://www.youtube.com/watch?v=HDUzBEG1GlA
+# https://github.com/joeyajames/Python/blob/master/graph_adjacency-matrix.py
+
+# implementation of an undirected graph using adjacency matrix
+class Vertex:
+  def __init__(self, n):
+    self.name = n
+    self.
+
+class Graph:
+  vertices = {}
+  edges = {}
+  edge_indices = {}
+
+
+# https://www.linkedin.com/pulse/beginners-guide-choropleth-map-python-visualize-chinas-ying-li/
+# for visualization of China on an actual map?
+
+
+
+
+# Python function for similarity metric 1
+# compare contents of 2 arrays assuming they have the same length
+def compare_sets_shared(a, b):
+  result = set(a).intersection(set(b))
+  return len(result)
+
+# Python function for similarity metric 2 
+# comparing the elements of 2 arrays from indices 3 to 9, assuming both have a length of 10
+def identical_4to10(a, b):
+  if (len(a) != len(b) or len(a) != 10):
+    return -1
+  tally = 0
+  for i in range(3, 10):
+    if a[i] == b[i]:
+      tally += 1
+  return tally
+
+# Python function for similarity metric 3
+# comparing the elements of 2 arrays from indices 1 to 2, assuming both have a length of 10
+def identical_2and3(a, b):
+  if (len(a) != len(b) or len(a) != 10):
+    return -1
+  tally = 0
+  for i in range(1, 2):
+    if a[i] == b[i]:
+      tally += 2
+  return tally
+
+
+# Python function for similarity metric 4
+# comparing the elements of 2 arrays from indices 1 to 2, assuming both have a length of 10
+def identical_1(a, b):
+  if (len(a) != len(b) or len(a) != 10):
+    return -1
+  if a[0] == b[0]:
+      return 5
+  return 0
